e8387a147dc6efabfe03690b6f7a50a4a85fe9e0
Peter2121 <peter@flytrace.com>
2015-03-17 23:31:39 +0100

    Some new parameters from Device.info page added to private.xml

--- OpenGTS/src/org/opengts/CompileTime.java
+++ OpenGTS/src/org/opengts/CompileTime.java
@@ -1,9 +1,9 @@
 package org.opengts;
 public class CompileTime
 {
-    // 2015/03/15 23:06:27 CET
-    public static final long   COMPILE_TIMESTAMP    = 1426457187L;
-    public static final String COMPILE_DATETIME     = "2015/03/15 23:06:27 CET";
+    // 2015/03/17 23:24:14 CET
+    public static final long   COMPILE_TIMESTAMP    = 1426631054L;
+    public static final String COMPILE_DATETIME     = "2015/03/17 23:24:14 CET";
     public static final String SERVICE_ACCOUNT_ID   = "opengts";
     public static final String SERVICE_ACCOUNT_NAME = "Open Source OpenGTS";
     public static final String SERVICE_ACCOUNT_KEY  = "";
