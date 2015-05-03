c74e43c6e36c385bc0b766eddc88fde7359a9312
Peter2121 <peter@flytrace.com>
2015-05-03 23:37:31 +0200

    Activation of fillFrame for map, styles modifications to improve this view.

--- OpenGTS/src/org/opengts/CompileTime.java
+++ OpenGTS/src/org/opengts/CompileTime.java
@@ -1,9 +1,9 @@
 package org.opengts;
 public class CompileTime
 {
-    // 2015/05/02 22:51:20 CEST
-    public static final long   COMPILE_TIMESTAMP    = 1430599880L;
-    public static final String COMPILE_DATETIME     = "2015/05/02 22:51:20 CEST";
+    // 2015/05/03 23:32:38 CEST
+    public static final long   COMPILE_TIMESTAMP    = 1430688758L;
+    public static final String COMPILE_DATETIME     = "2015/05/03 23:32:38 CEST";
     public static final String SERVICE_ACCOUNT_ID   = "opengts";
     public static final String SERVICE_ACCOUNT_NAME = "Open Source OpenGTS";
     public static final String SERVICE_ACCOUNT_KEY  = "";
