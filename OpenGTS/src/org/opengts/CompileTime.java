836ac6596a67a7971a38d3cf2f38c4543bc3f338
Peter2121 <peter@flytrace.com>
2016-12-11 23:17:31 +0100

    Improve Russian translation (work in progress)

--- OpenGTS/src/org/opengts/CompileTime.java
+++ OpenGTS/src/org/opengts/CompileTime.java
@@ -1,9 +1,9 @@
 package org.opengts;
 public class CompileTime
 {
-    // 2016/12/04 20:36:33 CET
-    public static final long   COMPILE_TIMESTAMP    = 1480880193L;
-    public static final String COMPILE_DATETIME     = "2016/12/04 20:36:33 CET";
+    // 2016/12/04 23:42:11 CET
+    public static final long   COMPILE_TIMESTAMP    = 1480891331L;
+    public static final String COMPILE_DATETIME     = "2016/12/04 23:42:11 CET";
     public static final String SERVICE_ACCOUNT_ID   = "opengts";
     public static final String SERVICE_ACCOUNT_NAME = "Open Source OpenGTS";
     public static final String SERVICE_ACCOUNT_KEY  = "";
